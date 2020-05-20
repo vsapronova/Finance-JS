f044f86919600fe1415e318996ddce68992ab308
Victoria Sapronova <v.sapronova@gmail.com>
2020-05-20 18:48:25 -0400

    Added script for rendering templates, renamed html pages

--- genhtml.py
+++ genhtml.py
@@ -1,5 +1,24 @@
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask
+import os
+from jinja2 import Environment, FileSystemLoader
 
-buy2 = render_template("buy2.html")
+app = Flask(__name__)
 
-print(buy2)
+env = Environment(loader=FileSystemLoader('templates'))
+
+rootdir = './templates'
+
+new_dir = './www'
+
+for file in os.listdir(rootdir):
+    template_path = os.path.join(rootdir, file)
+    print(template_path)
+    template = app.jinja_env.get_or_select_template(file)
+    output = template.render()
+
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+
+    output_path = os.path.join(new_dir, file)
+    with open(output_path, "w") as f:
+        f.write(output)
